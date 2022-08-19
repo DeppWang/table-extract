5870fe79a50855087fb6f4ba96bf2cac00ec029d
xingquanwang <xingquanwang@pharmaoryx.com>
2022-08-19 15:22:19 +0800

    update file

--- README.md
+++ README.md
@@ -1,105 +1,28 @@
-## 安装
-
-安装虚拟环境
-
-```
-python3 -m venv venv
-. venv/bin/activate
-```
-
-安装依赖
-
-```
-pip install -r requirements.txt
-```
-
-安装 `Comments`:
-
-```
-$ pip install -e .
-```
-
-## 运行
-
-
-```
-$ export FLASK_APP=comments
-$ flask init-db
-$ flask run
-```
-
-## 测试
-
-=======
 ## Hard Point
-1. 镜像
-2. Gunicorn 稳定性
+1. 镜像、Gunicorn 稳定性
+2. 图片解析
 3. 图片截取
-4. 图片解析
-5. 指定横向
+4. 指定横向
 
-
+## Docker 镜像
 
 ```
+git clone ssh://git@10.60.128.2:7999/elab-2/table-extract.git
 docker build -t table-extract .
 docker run -dit -p 80:80 --name=table-extract table-extract /bin/bash 
-cd workspace/table-extract
-docker cp . c0fd936bf027:/home/table-extract
+docker exec -it table-extract /bin/bash
 gunicorn -c gunicorn_conf.py tableetl:app --preload
-git clone ssh://git@10.60.128.2:7999/elab-2/table-extract.git
-docker exec -it c0fd936bf027 /bin/bash
+docker cp . table-extract:/home/
 ```
 
+## 表格提取、图片解析
 
+### 表格提取
 
-## 安装
-
-安装虚拟环境
-
-```
-python3 -m venv venv
-. venv/bin/activate
-```
-
-安装依赖
+### 图片解析
 
 ```
-pip install -r requirements.txt
+tesseract 26-1_1.jpg 26-1_1 -l eng pdf
+tesseract test.png test -l eng pdf
 ```
 
-## 运行
-
-### 内置开发模式：
-
-
-```
-$ export FLASK_APP=tableetl
-$ flask init-db
-$ flask run -h 0.0.0.0 -p 80
-```
-
-- https://flask.palletsprojects.com/en/2.0.x/server/#development-server
-
-### 保持后台运行
-
-```
-gunicorn -w 2 -b 0.0.0.0:80 tableetl:app -- daemon
-```
-
-- https://iximiuz.com/en/posts/flask-gevent-tutorial/
-- https://lionhead8.medium.com/run-python-flask-server-application-in-background-643692634fd3
-- https://medium.com/coding-memo/backend-run-flask-in-background-with-gunicorn-3f1f4cffca8d
-
-### Session
-
-
-
-## 测试
-
-安装 `tableetl`:
-
-```
-$ pip install -e .
-```
-
-执行测试
--- tableetl/index.py
+++ tableetl/index.py
@@ -4,41 +4,41 @@ from flask import Blueprint, current_app, flash, redirect, request, render_templ
 import tabula
 
 UPLOAD_FOLDER = '/home/table-extract/uploads'
-ALLOWED_EXTENSIONS = {'pdf'}
+ALLOWED_EXTENSIONS = {'pdf','png','jpg'}
 
 bp = Blueprint("index", __name__)
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         if 'file' not in request.files:
-            flash('No file part')
+            flash('请选择文件！')
             return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
+            flash('文件名称不能为空！')
             return redirect(request.url)
         
         if file and allowed_file(file.filename):
-            # filename = secure_filename(file.filename)
             input_file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-            output_file_path = os.path.join(UPLOAD_FOLDER, 'output.csv')
             file.save(input_file_path)
-            dfs = tabula.read_pdf(input_file_path, pages='all')
+            filename_prefix = file.filename.rsplit('.', 1)[0]
+            filename_suffix = file.filename.rsplit('.', 1)[1].lower()
+            if filename_suffix != 'pdf':
+                tess_cmd = 'sudo tesseract {} {} -l eng pdf'.format(file.filename, filename_prefix)
+                os.system(tess_cmd)
+            input_file_path = os.path.join(UPLOAD_FOLDER, filename_prefix + '.pdf')
+            output_file_path = os.path.join(UPLOAD_FOLDER, filename_prefix + '.csv')
             tabula.convert_into(input_file_path, output_file_path, output_format="csv", pages='all')
-            print('test2')
             uploads = os.path.join(current_app.root_path, UPLOAD_FOLDER)
             print(uploads)
-            # tabula.convert_into("test.pdf", "output.csv", output_format="csv", pages='all')
-            # return send_from_directory(directory=uploads, path='output.csv')
             return send_file(output_file_path, as_attachment=True)
-        flash('只能上传 pdf 文件')
+        flash('只能上传 PDF、PNG、JPG 格式文件！')
         return redirect(request.url)
         
     return render_template("tableetl/index.html")
